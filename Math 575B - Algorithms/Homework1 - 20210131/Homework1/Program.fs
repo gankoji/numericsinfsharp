508257879c8cf76e3cb19698e8f9d411b345a96a
Jake Bailey <asaxplayinghorse@gmail.com>
2021-01-30 01:23:00 -0700

    Bringing over projects to new repo.

---
+++ Math 575B - Algorithms/Homework1 - 20210131/Homework1/Program.fs
@@ -0,0 +1,123 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open FSharp.Plotly
+
+// Linear Regression
+let solveLinearLeastSquares (xs:list<double>) (ys:list<double>) =
+    let n = Seq.length xs
+    if not (n = (Seq.length ys)) then None
+    else
+        let xbar = Seq.average(xs)
+        let ybar = Seq.average(ys)
+        let xdiff = Seq.map (fun x -> x - xbar) xs
+        let ydiff = Seq.map (fun y -> y - ybar) ys
+        let xdiff2 = Seq.map (fun x -> x**2.0) xdiff
+        let xdyd = Seq.zip xdiff ydiff |> Seq.map (fun (x,y) -> x*y)
+        let yds = Seq.sum xdyd
+        let x2ds = Seq.sum xdiff2
+        let m = yds/x2ds
+        let b = (Seq.sum(ys) - (m*Seq.sum(xs)))/(double n)
+        Some(m, b)
+
+// Gradient Descent w/ Backtracking
+let gradientDescent f gradF x0 =
+    let rec descentLoop x (fval:double) dt = 
+        let newf = (f x)
+        let newg = (gradF x)
+        if (Math.Abs (newf - fval)) < 1e-10 then
+            printfn "Delta: %A" (Math.Abs (newf - fval))
+            x
+        else
+            let newx = Seq.zip x newg |> Seq.map (fun (x, dx) -> x - dt*dx)
+            let inter = (f newx)
+            if (inter > fval) then
+                descentLoop x (fval + 1.0) (0.5*dt)
+            else 
+                let newt = 1.1*dt
+                descentLoop newx newf newt
+
+    descentLoop x0 6000.0 0.01
+
+// Objective function for question 2
+let f2 (p:seq<double>) =
+    let x = Seq.item 0 p
+    let y = Seq.item 1 p
+    y**2.0 + (x - 10.0)**2.0 + 40.0*Math.Sqrt(-(x**2.0) + y**2.0 + Math.Sqrt((-(x**2.0) + y**2.0)**2.0 + 1.0))
+
+// Gradient for q2
+let g2 (p:seq<double>) = 
+    let x = Seq.item 0 p
+    let y = Seq.item 1 p
+    let sub1 = (-(x**2.0) + y**2.0)
+    let a = 
+        (2.0*x + 40.0*(-x*sub1/sqrt(sub1**2.0 + 1.0) - x)/Math.Sqrt(sub1 + Math.Sqrt(sub1**2.0 + 1.0)) - 20.0)
+    let b = 
+        (2.0*y + 40.0*(y*sub1/Math.Sqrt(sub1**2.0 + 1.0) + y)/Math.Sqrt(sub1 + Math.Sqrt(sub1**2.0 + 1.0)))
+
+    [a; b]
+
+// Objective function for q3
+let f3 (p:seq<double>) =
+    let x = Seq.item 0 p
+    let y = Seq.item 1 p
+    (x+3.0)**2.0 + (y**2.0)*Math.Exp(-2.0*x)
+
+// Gradient for q3
+let g3 (p:seq<double>) =
+    let x = Seq.item 0 p
+    let y = Seq.item 1 p
+    let a =
+        (2.0*x - 2.0*(y**2.0)*exp(-2.0*x) + 6.0)
+    let b =
+        (2.0*y*exp(-2.0*x))
+    [a; b]
+
+// Objective function for q4
+let f4 (p:seq<double>) =
+    let x = Seq.item 0 p
+    let y = Seq.item 1 p
+    (y**2.0)*exp(-2.0*x)/400.0 + (x + 3.0)**2.0
+    
+// Gradient for q4
+let g4 (p:seq<double>) =
+    let x = Seq.item 0 p
+    let y = Seq.item 1 p
+    let a =
+        (2.0*x - 2.0*(y**2.0)*exp(-2.0*x)/200.0 + 6.0)
+    let b =
+        (2.0*y*exp(-2.0*x)/200.0)
+    [a; b]
+
+// Pretty Printing for GradDesc probs
+let printGDResult f p (qnum:string) =
+    let v = (f p)
+    printfn "Question %s - Gradient Descent: %A" qnum f
+    printfn "Optimal Point: %A" p
+    printfn "Optimal Function Value: %A" v
+
+[<EntryPoint>]
+let main argv =
+
+    // Question 1
+    let xs = [-2.; -1.; 0.; 2.; 3.]
+    let ys = [-3.; -1.; 5.; 5.; 1.]
+    let q1 = solveLinearLeastSquares xs ys
+    printfn "Question 16.2 - Linear Regression: %A" q1
+
+
+    // Question 2
+    printfn "%A" (f2 [-50.0; 40.0])
+    let optPoint = gradientDescent f2 g2 [-50.0; 40.0]
+    printGDResult f2 optPoint "17.1a"
+
+    // Question 3
+    printfn "17.1b Start: %A" (f3 [0.0; 1.0])
+    let optPoint = gradientDescent f3 g3 [0.0; 1.0]
+    printGDResult f3 optPoint "17.1b"
+
+    // Question 4
+    printfn "17.1c Start: %A" (f4 [0.0; 20.0])
+    let optPoint = gradientDescent f4 g4 [0.0; 20.0]
+    printGDResult f4 optPoint "17.1c"
+    0 // return an integer exit code
--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # numericsinfsharp
-This is the home of my work for the 2021 spring semester, focusing on convex analysis, measure and probability, and optimization. Written mostly in F#, with some Python for verification.
+
+This is the home of my work for the 2021 spring semester, focusing on convex
+analysis, measure and probability, and optimization. Written mostly in F#, with
+some Python for verification.
+
+## Okay, maybe more than 'some' 
+
+Truth be told, I have almost zero idea what I'm doing with F#. I have some
+experience with SMLNJ from an online course I did last summer, which thankfully
+means I don't have to *completely* transition into FP, and have a leg up on
+starting with F# since it's a member of the 'ML Family.' That said, I will be
+*starting* with F# in this project: I have no prior experience. That's why the
+tagline says 'some Python for verification.'
+
+I've been doing numerical/scientific programming, particularly with Python, for
+years now in my professional life. So, as a twist to the work I'm doing for my
+classes, which cover mostly topics I've worked with already, I thought I'd take
+this opportunity to learn a new language. However, I'll quite often need to fall
+back on Python for things like SymPy, and SciPy.optimize.
+
+## Should Be Fun!
+
+All of the above notwithstanding, the primary purpose of this repo is to catalog
+the work I'm doing on learning F#, and to have a convenient place for me to
+refer to in the future in the event I find I need a quick way to solve
+GradientDescent while hacking away at something else on the CLR. 
+
+## One Final Note
+
+About building this code. Should you, for some absurd reason, feel the need or
+desire to want to run any of this code, you have a couple of options. I'm doing
+this work on a Win10 machine with Visual Studio, so each assignment or
+mini-project will be organized as such. You can also run any of the F# code on
+Linux or Mac OSX, via .NET Core, although I leave package management up to you.
+Python is python, although I should note that my deps for Python generally
+include Python 3 (because its not 2005 anymore), the SciPy stack (SciPy, NumPy,
+Pandas and Matplotlib) and SymPy. If you're on Linux or Mac OSX, you have pip,
+so those are easily handled. If you're on windows like me, I highly recommend
+going with [Anaconda](https://www.anaconda.com/).
