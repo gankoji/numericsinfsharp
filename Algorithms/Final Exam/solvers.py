bd4517e592fa1a496f34777ac97551f8d5df222f
Jake Bailey <asaxplayinghorse@gmail.com>
2021-05-13 09:19:42 -0700

    Tidy images

--- Algorithms/Final Exam/solvers.py
+++ Algorithms/Final Exam/solvers.py
@@ -28,31 +28,22 @@ def newtonIneq(f,gs, dF, ddF, xin, tol):
     x = xin
     t = 1.
     while (t < 1e14):
-        #print("Increasing t")
         cur_dF = dF(x, t)
         while (np.linalg.norm(cur_dF) > tol*math.sqrt(t)):
-            #print("Stepping")
             d = -np.linalg.solve(ddF(x,t),cur_dF)
             nx = x+d
             gvals = np.array([g(nx[0],nx[1],nx[2],t) for g in gs])
             while(np.any(gvals > 0)):
-                #print("Halving step")
                 d = d/2.
                 nx = x+d
                 gvals = np.array([g(nx[0],nx[1],nx[2],t) for g in gs])
             cur_F = f(x,t)
             while (f(x+d,t) >= cur_F):
-                # print("Halving step for fun")
-                # print(d)
-                # print(f(x+d,t))
-                # print(cur_F)
                 d = d/2.
                 if np.linalg.norm(d) <= tol:
                     break
             x = x+d
             cur_dF = dF(x,t)
         t = t*2.
-    
+
     return x
-
-
