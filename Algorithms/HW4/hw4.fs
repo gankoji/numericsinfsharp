2079a5b7324effbec8cce5369c05fc2cf6fbbc50
Jake Bailey <asaxplayinghorse@gmail.com>
2021-05-15 23:17:24 -0700

    HW4 coming together, random walk done.

--- Algorithms/HW4/hw4.fs
+++ Algorithms/HW4/hw4.fs
@@ -59,9 +59,9 @@ let adjacencyMatrix = array2D [[0; 0; 0; 0; 1; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0];
                                [0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 1; 0; 1];
                                [0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 1; 0; 0; 1; 0]]
 
-let findAdjacents node (adjMat:array<int> [,]) =
+let findAdjacents node (adjMat:int [,]) =
     let adjList = (Array.toList adjMat.[node,*])
-    let rec searchList list acc =
+    let rec searchList (list:List<int>) acc =
         match list with
             a::b -> if (a > 0) then
                         acc::(searchList b (acc + 1))
@@ -69,17 +69,67 @@ let findAdjacents node (adjMat:array<int> [,]) =
                         (searchList b (acc + 1))
           | _ -> []
 
-    printfn "%A" adjList
-    //(searchList adjList 0)
+    (searchList adjList 0)
+
+let chooseMove (adjacent:List<int>) =
+    let n = adjacent.Length
+    let u = rand.Next(n)
+    List.item u adjacent
+
+    // def randomWalkOnGraph(start, adjMat):
+    //     terminated = False
+    //     special = [0, 1] # Cheese is at node 0, cat at node 1
+    //     current = start
+    //     while (not terminated):
+    //         adjacents = findAdjacents(current, adjMat)
+    //         nextNode = chooseMove(adjacents)
+    //         if (nextNode in special):
+    //             terminated = True
+    //             if nextNode == 0:
+    //                 return 1
+    //             else:
+    //                 return 0
+    //         current = nextNode
+
+let randomWalkOnGraph start (adjMat:int [,]) =
+    let special = [0;1]
+    let rec walkGraph current =
+        let adjacents = (findAdjacents current adjMat)
+        let nextNode = (chooseMove adjacents)
+        // if (nextNode in special)
+        if (List.contains nextNode special) then
+            if (nextNode = 0) then
+                1
+            else
+                0
+        else
+            (walkGraph nextNode)
+    (walkGraph start)
+
+let monteCarloWalk (nSamples:int) =
+    // Use oneSample above to get to nSamples
+    let samples = []
+    let rec sample samples acc =
+        if acc = nSamples then
+            samples
+        else
+            (sample (List.append [(randomWalkOnGraph 15 adjacencyMatrix)] samples) (acc + 1))
+    (sample samples 0)
 
 [<EntryPoint>]
 let main argv =
+    let stopwatch = System.Diagnostics.Stopwatch.StartNew()
     // Problem 23.2: Monte-Carlo sampling of
     // an arbitrary distribution
-    printfn "%A" (monteCarloSamples 50)
+    printfn "%A" (monteCarloSamples (int 1e6))
     // While the code is somewhat different (recursion vs looping)
     // They're very similar lengths: 25 lines in python, 21 in F#
 
     // Next up: 24.1, a mouse's random walk 
-    printfn "%A" adjacencyMatrix.[1,*]
+    let nRuns = 1000000
+    let successes = (List.sum (monteCarloWalk nRuns))
+
+    stopwatch.Stop()
+    printfn "Probability that mouse survivies: %A" ((float successes)/(float nRuns))
+    printfn "Elapsed time: %f" stopwatch.Elapsed.TotalSeconds
     0 // return an integer exit code
